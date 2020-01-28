4f0513c22686dd03ab881a123759151596b2798a
lab302-35 <lab302-35@labeng.local>
2020-01-28 16:59:56 +0700

    A

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,23 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0 , z = 0;
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source,textline))
+    {
+        sum += atof(textline.c_str());
+        z += pow(atof(textline.c_str()),2);
+        count++;
+    }
+    cout << "Number of data = "<< count << "\n";
+    cout << "Mean = "<< sum/count << "\n";
+    cout << "Standard deviation = "<< sqrt((z/count)-pow(sum/count,2));
+    return 0;
+}
+
